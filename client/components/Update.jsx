cbfe5ecbb097afd285dd2f9defa3df90017d1de1
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-10-22 18:22:05 +1300

    Refactor Update Component

--- client/components/Update.jsx
+++ client/components/Update.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router'
 
 import Form from './Form'
 
@@ -9,19 +8,10 @@ const Update = () => {
   const updatedOpenMic = useSelector((state) => state.oneOpenMicRed)
   const token = useSelector((state) => state.authReducer)
 
-  const id = useParams()
-
-  console.log(updatedOpenMic)
-
-  const openMicId = displayUpdate.id
-  console.log('Update Id: ', openMicId)
-
   // Authorization
   const [authorization, setAuthorization] = useState(false)
 
   const authorize = () => {
-    console.log('Open Mic AuthID: ', updatedOpenMic?.auth_token)
-    console.log('Updated Token: ', token)
     if (updatedOpenMic?.auth_token === token) setAuthorization(true)
   }
 
