262f53d4cd995d306b0ab8a928a03fbffc7745d2
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-10-24 11:34:40 +1300

    Refactor backend

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,3 @@
-// const config = require('./knexfile').development
-// const conn = require('knex')(config)
-
 const knex = require('knex')
 const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
@@ -23,7 +20,6 @@ function getTheOpenMic(id, db = conn) {
 }
 
 function deleteOneOpenMic(id, db = conn) {
-  console.log('DB: ', id, typeof id)
   return db('openMics').del().where('id', id)
 }
 
